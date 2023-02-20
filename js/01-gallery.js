8fcfe69ca4b88f448385293a48b1c084060a1b42
Taras-Veremchuk <taras.veremm@gmail.com>
2023-02-20 01:47:19 +0100

    add 1 party

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createImagesGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+const myGallery = createImagesGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', myGallery);
+
+gallery.addEventListener('click', onMyGalleryElClick);
+
+function onMyGalleryElClick(e) {
+  e.preventDefault();
+  const isCorrectClick = e.target.nodeName === 'IMG';
+  if (!isCorrectClick) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="1280" height="720">
+`);
+  onOpenModal(instance);
+  onCloseModal(instance);
+
+  console.log(e.target.dataset.source);
+}
+
+function onOpenModal(modal) {
+  modal.show();
+}
+function onCloseModal(modal) {
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      modal.close();
+    }
+  });
+}
